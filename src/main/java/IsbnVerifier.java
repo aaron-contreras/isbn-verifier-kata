7a2b6c81c0dfab181ba6d41bb343ce71b7c1a5a7
Aaron Contreras <aaronlcaq@gmail.com>
2020-03-18 20:29:27 -0500

    First solution

--- src/main/java/IsbnVerifier.java
+++ src/main/java/IsbnVerifier.java
@@ -1,7 +1,33 @@
 class IsbnVerifier {
 
     boolean isValid(String stringToVerify) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        String cleanedString = stringToVerify.replaceAll("-", "");
+
+        if (cleanedString.length() != 10) return false;
+
+        int checkCharacter = cleanedString.contains("X") ? 10 : Character.getNumericValue(cleanedString.charAt(9));
+
+        if (checkCharacter > 9 && cleanedString.charAt(9) != 'X') {
+            return false;
+        }
+
+        int checksum = 0;
+
+        for (int i = 0, multiplier = 10; i < cleanedString.length(); i++, multiplier --) {
+            
+            int currentDigit = 0;
+            
+            if (i == 9) {
+                currentDigit = checkCharacter;
+            } else {
+                currentDigit = Character.getNumericValue(cleanedString.charAt(i));
+            }
+            checksum += currentDigit * multiplier;
+        }
+
+
+        return checksum % 11 == 0;
     }
 
 }
